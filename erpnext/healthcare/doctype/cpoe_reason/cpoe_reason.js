6909b8530a70db4b41b27621d0550fa2c153ec44
anoop <anoop@earthianslive.com>
2020-07-24 22:54:16 +0530

    feat(wip): CPOE doctype and related masters

---
+++ erpnext/healthcare/doctype/cpoe_reason/cpoe_reason.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, earthians and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('CPOE Reason', {
+	// refresh: function(frm) {
+
+	// }
+});
